b0394410ffaa5413d2c8f2545f64645a0b381102
Eyal Lustig <eyallustig@gmail.com>
2025-11-17 13:52:50 +0200

    Add logs to the web app

--- main.go
+++ main.go
@@ -3,37 +3,75 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	// Render the home html page from static folder
+	log.Printf("Request: %s %s from %s", r.Method, r.URL.Path, r.RemoteAddr)
+	start := time.Now()
+	defer func() {
+		log.Printf("Completed %s in %v", r.URL.Path, time.Since(start))
+	}()
 	http.ServeFile(w, r, "static/home.html")
 }
 
 func coursePage(w http.ResponseWriter, r *http.Request) {
-	// Render the course html page
+	log.Printf("Request: %s %s from %s", r.Method, r.URL.Path, r.RemoteAddr)
+	start := time.Now()
+	defer func() {
+		log.Printf("Completed %s in %v", r.URL.Path, time.Since(start))
+	}()
 	http.ServeFile(w, r, "static/courses.html")
 }
 
 func aboutPage(w http.ResponseWriter, r *http.Request) {
-	// Render the about html page
+	log.Printf("Request: %s %s from %s", r.Method, r.URL.Path, r.RemoteAddr)
+	start := time.Now()
+	defer func() {
+		log.Printf("Completed %s in %v", r.URL.Path, time.Since(start))
+	}()
 	http.ServeFile(w, r, "static/about.html")
 }
 
 func contactPage(w http.ResponseWriter, r *http.Request) {
-	// Render the contact html page
+	log.Printf("Request: %s %s from %s", r.Method, r.URL.Path, r.RemoteAddr)
+	start := time.Now()
+	defer func() {
+		log.Printf("Completed %s in %v", r.URL.Path, time.Since(start))
+	}()
 	http.ServeFile(w, r, "static/contact.html")
 }
 
+func loggingMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Incoming request: %s %s from %s", r.Method, r.URL.Path, r.RemoteAddr)
+		next.ServeHTTP(w, r)
+	}
+}
+
 func main() {
+	// Set up file server for static files
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	http.HandleFunc("/home", homePage)
-	http.HandleFunc("/courses", coursePage)
-	http.HandleFunc("/about", aboutPage)
-	http.HandleFunc("/contact", contactPage)
+	// Routes with logging middleware
+	http.HandleFunc("/home", loggingMiddleware(homePage))
+	http.HandleFunc("/courses", loggingMiddleware(coursePage))
+	http.HandleFunc("/about", loggingMiddleware(aboutPage))
+	http.HandleFunc("/contact", loggingMiddleware(contactPage))
 
-	err := http.ListenAndServe("0.0.0.0:8080", nil)
+	// Log server start
+	serverAddr := "0.0.0.0:8080"
+	log.Printf("Server starting on http://%s", serverAddr)
+	log.Printf("Available endpoints:")
+	log.Printf("  - http://%s/home", serverAddr)
+	log.Printf("  - http://%s/courses", serverAddr)
+	log.Printf("  - http://%s/about", serverAddr)
+	log.Printf("  - http://%s/contact", serverAddr)
+
+	// Start server
+	err := http.ListenAndServe(serverAddr, nil)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Server failed to start: %v", err)
 	}
 }
